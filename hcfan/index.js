e92551094ad564b11ee134c24fea71a76e53f204
Hsieh Chin Fan <pham@topo.tw>
2023-09-06 12:54:45 +0800

    Improve html sementic
    * Rename variables in CSS
    * Add html comments
    * Move <button> out of <h1>
    * Add onmouseover event for carousel

--- hcfan/index.js
+++ hcfan/index.js
@@ -62,3 +62,9 @@ window.addEventListener("keydown", (event) => {
             break;
     }
 }, true,);
+
+function showCaption(display) {
+    document.querySelectorAll(".carousel-caption").forEach(element => {
+       element.style.opacity = display ? 1 : 0; 
+    });
+}
